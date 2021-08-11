98a9db0afd8bb7603dfc2f6c361651ba990eeb59
Friedrich Clausen <friedrich.clausen@blackboard.com>
2021-08-11 15:30:46 +1000

    Try and get upstream breanch

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,6 +13,13 @@ properties([
   ])
 ])
 
+def cause = currentBuild.getBuildCauses('hudson.model.Cause$UpstreamCause')
+if ( cause ) {
+  echo "We have a cause!"
+  def remoteBranch = cause.upstreamRun().getEnvironment().GIT_BRANCH
+  echo "Remove branch: $remoteBranch"
+}
+
 echo "Waits on: sandbox/fclausen/jenkins-k8s-hello-world/${URLEncoder.encode( env.BRANCH_NAME, 'UTF-8')}"
 // URLEncoder.encode(toEncode, "UTF-8")
 echo "I have run with $env.SOME_PARAM"
