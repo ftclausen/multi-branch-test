dcb612a46dff0d9a34b4da1fc931a9f8f2ff503f
Friedrich Clausen <friedrich.clausen@blackboard.com>
2021-08-12 13:05:41 +1000

    Some debugging

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -11,16 +11,14 @@ properties([
   pipelineTriggers( getParentBranches( 'sandbox/fclausen/jenkins-k8s-hello-world' ) ),
 ])
 
-def cause = currentBuild.rawBuild.getCause(hudson.model.Cause.UpstreamCause)
-if ( cause ) {
-  echo "We have a cause!"
-  def remoteBranch = cause.getUpstreamRun().getEnvironment().BRANCH_NAME
-  echo "Remote branch: $remoteBranch"
+stage( 'Stuff' ) {
+  def cause = currentBuild.rawBuild.getCause(hudson.model.Cause.UpstreamCause)
+  if ( cause ) {
+    echo "We have a cause!"
+    def remoteBranch = cause.getUpstreamRun().getEnvironment().BRANCH_NAME
+    echo "Remote branch: $remoteBranch"
+  }
 }
-
-echo "Waits on: sandbox/fclausen/jenkins-k8s-hello-world/${URLEncoder.encode( env.BRANCH_NAME, 'UTF-8')}"
-// URLEncoder.encode(toEncode, "UTF-8")
-echo "I have run with $env.SOME_PARAM"
 
 def getParentBranches( def jobName ) {
   def jobs = [ upstream( "$jobName/${URLEncoder.encode( env.BRANCH_NAME, 'UTF-8' )}" ) ]
@@ -29,5 +27,6 @@ def getParentBranches( def jobName ) {
     jobs += upstream( "$jobName/${URLEncoder.encode( 'preDevelop', 'UTF-8' )}" )
   }
 
+  echo "DEBUG: Returning: $jobs"
   return jobs
 }
